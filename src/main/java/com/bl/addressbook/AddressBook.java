673dc3a2cced548a8ebbd1f7794fc9f2ee153a39
kalevishal <kalevishal1610@gmail.com>
2021-11-05 15:38:56 +0530

    Added: Ability to count number of contacts based on city or state

--- src/main/java/com/bl/addressbook/AddressBook.java
+++ src/main/java/com/bl/addressbook/AddressBook.java
@@ -1,3 +1,5 @@
+package com.bl.addressbook;
+
 import java.io.*;
 import java.sql.SQLException;
 import java.util.List;
@@ -5,12 +7,27 @@ import java.util.Scanner;
 
 public class AddressBook {
     AddressBookRepo addressBookRepo = new AddressBookRepo();
+    Scanner scanner=new Scanner(System.in);
 
     public static void main(String[] args) throws SQLException {
         AddressBook addressBook=new AddressBook();
         addressBook.retrieveData();
         addressBook.updateSalary();
         addressBook.getContactsInDatePeriod();
+        addressBook.countByCity();
+        addressBook.countByState();
+    }
+
+    private void countByState() {
+        System.out.println("Enter state name");
+        int stateContactsCount=  addressBookRepo.countByState(scanner.next());
+        System.out.println("Number of Contacts is Given state= " + stateContactsCount);
+    }
+
+    private void countByCity() {
+        System.out.println("Enter city Name");
+        int cityContactsCount = addressBookRepo.countByCity(scanner.next());
+        System.out.println("Number of Contacts is Given city= " + cityContactsCount);
     }
 
     private void getContactsInDatePeriod() throws SQLException {
--- src/main/java/com/bl/addressbook/AddressBookRepo.java
+++ src/main/java/com/bl/addressbook/AddressBookRepo.java
@@ -1,3 +1,5 @@
+package com.bl.addressbook;
+
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -8,7 +10,7 @@ public class AddressBookRepo {
     static Connection getConnection() {
         String url = "jdbc:mysql://localhost:3306/AddressBookDB";
         String username = "root";
-        String password = "Monali@12345";
+        String password = "9021";
         Connection connection = null;
         try {
             //Loading and registering driver
@@ -96,4 +98,34 @@ public class AddressBookRepo {
             System.out.println(rowData + " \n ");
         }
     }
+    public int countByCity(String city) {
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "select count(firstname) from AddressBook where city=" + "'" + city + "';";
+            ResultSet result = statement.executeQuery(sql);
+            result.next();
+            int count = result.getInt(1);
+
+
+            return count;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int countByState(String state) {
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "select count(firstname) from AddressBook where city=" + "'" + state + "';";
+            ResultSet result = statement.executeQuery(sql);
+            result.next();
+            int count = result.getInt(1);
+            return count;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
 }
